62bcf8acc4ee9d9cd34ff82521191a7517dc86d6
infernalslam <infernal-slam@hotmail.com>
2017-01-09 06:31:01 +0700

    mode

--- todo.js
+++ todo.js
@@ -1,6 +1,18 @@
-/* global angular */
+/* global angular, rangy */
 angular.module('todoApp', [])
   .controller('TodoListController', function () {
     var app = this
-    app.test = 'เรารักหมามิ้น'
+    app.test = 'เรารักหมามิ้น I love you so much'
+    app.color = 'background: #202328; color: white;'
+    app.mode = 'light'
+
+    app.changeMode = function (mode) {
+      if (mode === 'dark') {
+        app.color = 'background: #202328; color: white;'
+        app.mode = 'light'
+      } else if (mode === 'light') {
+        app.color = 'background: #d6d6d6; color: black;'
+        app.mode = 'dark'
+      }
+    }
   })
