8ddbea97e1eebe8ea0e00fc13f079d4b89499eea
infernalslam <infernal-slam@hotmail.com>
2017-01-10 15:00:09 +0700

    fix add content

--- todo.js
+++ todo.js
@@ -1,10 +1,11 @@
-/* global angular, rangy */
+/* global angular */
 angular.module('todoApp', [])
   .controller('TodoListController', function () {
     var app = this
     app.test = 'เรารักหมามิ้น I love you so much'
     app.color = 'background: #202328; color: white;'
     app.mode = 'light'
+    // $localStorage.test = '12345'
 
     app.changeMode = function (mode) {
       if (mode === 'dark') {
