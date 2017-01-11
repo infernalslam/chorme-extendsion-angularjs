75d422ae96c26acbba90d9fb50c27b3d55461e21
infernalslam <infernal-slam@hotmail.com>
2017-01-11 14:14:25 +0700

    add

--- todo.js
+++ todo.js
@@ -2,18 +2,5 @@
 angular.module('todoApp', [])
   .controller('TodoListController', function () {
     var app = this
-    app.test = 'เรารักหมามิ้น I love you so much'
-    app.color = 'background: #202328; color: white;'
-    app.mode = 'light'
-    // $localStorage.test = '12345'
-
-    app.changeMode = function (mode) {
-      if (mode === 'dark') {
-        app.color = 'background: #202328; color: white;'
-        app.mode = 'light'
-      } else if (mode === 'light') {
-        app.color = 'background: #d6d6d6; color: black;'
-        app.mode = 'dark'
-      }
-    }
+    app.simple = 'MY Dogs'
   })
